64a5ff0fe6efb910454b3aa94ff8bad7b2535ac2
iTitou <titouanchristophe@gmail.com>
2013-11-16 20:58:56 +0100

    [enh] Config management + exception handling

--- lechbot.rb
+++ lechbot.rb
@@ -48,12 +48,12 @@ lechbot = Cinch::Bot.new do
     config.plugins.plugins = [StatusBot, MotdBot, TwitterBot]
     @last_motd = nil
 
-    Twitter.configure do |config|
-      config.consumer_key = TWITTER_CONSUMER_KEY
-      config.consumer_secret = TWITTER_CONSUMER_SECRET
-      config.oauth_token = TWITTER_OAUTH_TOKEN
-      config.oauth_token_secret = TWITTER_OAUTH_SECRET
-    end
+    conf.plugins.options[TwitterBot] = {
+      consumer_key: TWITTER_CONSUMER_KEY,
+      consumer_secret: TWITTER_CONSUMER_SECRET,
+      oauth_token: TWITTER_OAUTH_TOKEN,
+      oauth_token_secret: TWITTER_OAUTH_SECRET 
+    }
   end
     
   #Explain the meaning of Lechbot's life
--- plugins/twitter.rb
+++ plugins/twitter.rb
@@ -9,32 +9,58 @@ class TwitterBot
 
 	TWITTER_URL = /.*https?:\/\/(mobile\.)?twitter\.com\/[^\/]+\/status\/(\d+)?/
 
+	listen_to :connect, :method => :configureTwitter
+	def configureTwitter msg
+		Twitter.configure do |conf|
+      		conf.consumer_key = (config[:consumer_key] || raise(ArgumentError, "Missing Twitter consumer key"))
+      		conf.consumer_secret = (config[:consumer_secret] || raise(ArgumentError, "Missing Twitter consumer secret"))
+      		conf.oauth_token = (config[:oauth_token] || raise(ArgumentError, "Missing Twitter OAuth token"))
+      		conf.oauth_token_secret = (config[:oauth_token_secret] || raise(ArgumentError, "Missing Twitter OAuth token secret"))
+    	end
+    	bot.info "Twitter configured (#{config[:consumer_key]}) !"
+	end
+
 	def url2tweetid url
 		$2.to_i if url =~ TWITTER_URL
 	end
 
+	def protectTwitter msg
+		begin
+			yield
+		rescue Twitter::Error::AlreadyRetweeted
+			msg.reply "Déjà retweeté"
+		rescue Twitter::Error::Unauthorized, Twitter::Error::Forbidden
+			msg.reply "Non-autorisé"
+		rescue Twitter::Error::NotFound
+			msg.reply "Tweet introuvable..."
+		end
+	end
+
 	match TWITTER_URL, :method => :showTweet, :prefix => //
 	def showTweet msg
-		debug "Tweet ID: #{url2tweetid msg.message}"
-		tweet = Twitter.status(url2tweetid msg)
+		tweet = Twitter.status(url2tweetid msg.message)
 		msg.reply "@#{tweet.from_user}: «#{tweet.text}»"
 	end
 
-	match /twitter\s*(\s!.+)/, :method => :postTweet
+	match /twitter\s(.+)/, :method => :postTweet
 	def postTweet msg, text
-		Twitter.update(text)
-	    msg.reply "Pinky Pinky"
+		text.strip!
+		debug text
+		if text.empty? 
+			msg.reply "Pas grand chose à dire..."
+		else
+			protectTwitter msg do
+				Twitter.update(text)
+				msg.reply "Pinky Pinky !"
+			end
+		end
 	end
 
 	match /retweet\s+#{TWITTER_URL}/, :method => :reTweet
 	def reTweet msg
-		begin
+		protectTwitter msg do 
 			tweet = Twitter.retweet(url2tweetid msg.message)
-			msg.reply "Bowel !"
-		rescue Twitter::Error::AlreadyRetweeted
-			msg.reply "Déjà retweeté"
-		rescue Twitter::Error::Unauthorized
-			msg.reply "Non-autorisé"
+			msg.reply "Bowel Bowel !"
 		end
 	end
 end
