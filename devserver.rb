1d026e7f4aa9cb8275ed21e81ee02b2b9116d5ce
iTitou <titouanchristophe@gmail.com>
2013-11-17 22:04:54 +0100

    [fix] Get status should return "closed"

--- devserver.rb
+++ devserver.rb
@@ -6,7 +6,7 @@ $status = 'closed'
 
 get '/changestatus' do
   if params.key?('status') && ['open', 'close'].include?(params['status'])
-    $status = params['status']
+    $status = (params['status'] == 'open') ? 'open' : 'closed'
     return 200, $status
   end
   return 400, "Unknown status"
