0445d4b68b8e1c5f074eda71fb367ac50c64a357
iTitou <titouanchristophe@gmail.com>
2013-11-26 00:30:11 +0100

    [fix] Respond in json

--- devserver.rb
+++ devserver.rb
@@ -1,17 +1,20 @@
 # Development Open/Close API endpoint
 
 require 'sinatra'
+require 'json'
 
 $status = 'closed'
+$lastchange = Time.now
 
 get '/changestatus' do
   if params.key?('status') && ['open', 'close'].include?(params['status'])
+  	$lastchange = Time.now
     $status = (params['status'] == 'open') ? 'open' : 'closed'
-    return 200, $status
+    return {'state' => $status, 'since' => $lastchange.to_i}.to_json
   end
   return 400, "Unknown status"
 end
 
 get '/getstatus' do
-  return $status
+  return {'state' => $status, 'since' => $lastchange.to_i}.to_json
 end
