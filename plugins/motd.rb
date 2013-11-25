4152e34b86a02d8cf3e5e228f634478a28a03b45
iTitou <titouanchristophe@gmail.com>
2013-11-26 00:29:12 +0100

    [fix] Motd was broken + split code in submethods

--- plugins/motd.rb
+++ plugins/motd.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+require 'cinch'
 require 'mechanize'
 
 class Time
@@ -9,8 +10,39 @@ class Time
 end
 
 
+module MediaWiki
+    def login wiki_url, user, pass
+        #Build a new browser
+        agent = Mechanize.new do |ag|
+            ag.follow_meta_refresh = true
+        end
+      
+        #Go to homepage
+        wiki_URI = URI.parse wiki_url.to_s
+        homepage = agent.get "http://#{wiki_URI.host}/"
+        loginpage = agent.click homepage.link_with(:text => /log in/i)
+        loggedhome = loginpage.form_with(:name => 'userlogin'){|form|
+            form.wpName  = user
+            form.wpPassword = pass
+        }.submit
+        return agent
+    end
+
+    def updateMotD agent, wiki_url, author, music_url
+        mypage = agent.get wiki_url
+
+        editpage = agent.click mypage.link_with(:text => /edit/i)
+        #We have the edit page
+        donepage = editpage.form_with(:name => 'editform'){|form|
+            pubtime = Time.now.strftime "%d/%m/%Y a %H:%M"
+            form.wpTextbox1 += "\n*#{author} #{music_url} (#{pubtime})"
+        }.submit
+    end
+end
+
 class MotdBot
     include Cinch::Plugin
+    include MediaWiki
 
     listen_to :topic, :method => :changeMotd
     def changeMotd msg
@@ -19,39 +51,15 @@ class MotdBot
         elsif @last_motd && @last_motd.same_day?(Time.now)
             msg.reply "Le MotD a déjà été changé aujourd'hui !"
         elsif ! config[:motd_wiki_url] || config[:motd_wiki_url].empty?
-            msg.reply "L'URL du MotD manque dans la configuration"
+            bot.error "L'URL du MotD manque dans la configuration"
         elsif ! config[:username] || config[:username].empty?
-            msg.reply "Nom d'utilisateur sur le wiki manquant"
+            bot.error "Nom d'utilisateur sur le wiki manquant"
         elsif ! config[:password] || config[:password].empty?
-            msg.reply "Mot de passe du wiki manquant"
-        else
+            bot.error "Mot de passe du wiki manquant"
+        else    
             url = URI.parse $1
-          
-            #Build a new browser
-            agent = Mechanize.new do |ag|
-                ag.follow_meta_refresh = true
-            end
-          
-            #Go to homepage
-            wiki_URI = URI.parse config[:motd_wiki_url]
-            homepage = agent.get "http://#{wiki_URI.host}/"
-            loginpage = agent.click homepage.link_with(:text => /log in/i)
-            loggedhome = loginpage.form_with(:name => 'userlogin'){|form|
-                form.wpName  = config[:username],
-                form.wpPassword = config[:password]
-            }.submit
-          
-            #We're now logged in
-          
-            mypage = agent.get config[:motd_wiki_url]
-            editpage = agent.click mypage.link_with(:text => /edit/i)
-            #We have the edit page
-            donepage = editpage.form_with(:name => 'editform'){|form|
-                pubtime = Time.now.strftime "%d/%m/%Y a %H:%M"
-                #Adding an entry (new music)
-                form.wpTextbox1 = form.wpTextbox1 + "\n*#{msg.user} #{url} (#{pubtime})"
-            }.submit
-          
+            agent = login config[:motd_wiki_url], config[:username], config[:password]
+            updateMotD agent, config[:motd_wiki_url], msg.user, url
             #Say something on the chan
             msg.reply "#{msg.user} gagne le MotD !"
             @last_motd = Time.now
