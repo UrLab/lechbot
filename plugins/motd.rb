38b6df2c186607f10527667786155a2810147794
iTitou <titouanchristophe@gmail.com>
2014-01-17 16:07:28 +0100

    [ref] Use !topic et !motd instead of /topic

--- plugins/motd.rb
+++ plugins/motd.rb
@@ -44,25 +44,70 @@ class MotdBot
     include Cinch::Plugin
     include MediaWiki
 
-    listen_to :topic, :method => :changeMotd
-    def changeMotd msg
-        if not msg.message =~ /^\s*(https?:\/\/[^\s]+)/
-            msg.reply "Pas d'url, pas de MotD d°-°b"
-        elsif @last_motd && @last_motd.same_day?(Time.now)
-            msg.reply "Le MotD a déjà été changé aujourd'hui !"
-        elsif ! config[:motd_wiki_url] || config[:motd_wiki_url].empty?
-            bot.error "L'URL du MotD manque dans la configuration"
-        elsif ! config[:username] || config[:username].empty?
-            bot.error "Nom d'utilisateur sur le wiki manquant"
-        elsif ! config[:password] || config[:password].empty?
-            bot.error "Mot de passe du wiki manquant"
-        else    
-            url = URI.parse $1
-            agent = login config[:motd_wiki_url], config[:username], config[:password]
-            updateMotD agent, config[:motd_wiki_url], msg.user, url
-            #Say something on the chan
-            msg.reply "#{msg.user} gagne le MotD !"
-            @last_motd = Time.now
+    STATEFILE = 'motd.yml'
+
+    def withState
+        state = {}
+        if File.exists? STATEFILE
+            state = YAML.load File.read(STATEFILE)
+        else
+            state = {
+                music: {
+                    text: "http://www.youtube.com/watch?v=p8oi6M4z_e0",
+                    changed: Time.at(0)
+                }, 
+                topic: {
+                    text: "Brace yourself, LechBot is coming !!!",
+                    changed: Time.at(0)
+                }
+            }
+        end
+        yield state
+        File.write STATEFILE, YAML.dump(state)
+    end
+
+    def makeTopic state, chan
+        chan.topic = "#{state[:music][:text]} :: #{state[:topic][:text]}"
+    end
+
+    listen_to :topic, :method => :remakeTopic
+    def remakeTopic msg
+        return if msg.user == bot.name
+        withState do |state|
+            makeTopic state, msg.channel
+        end
+        msg.reply "#{msg.user}: *JE* m'occupe du MotD !!! (utilise !motd et/ou !topic)"
+    end
+
+    match /motd\s*(https?:\/\/[^\s]+)/, :method => :changeMotd
+    def changeMotd msg, url
+        withState do |state|
+            if state[:music][:changed].to_date == Time.now.to_date
+                msg.reply "La musique du jour a déjà été changée aujourd'hui !"
+            elsif ! config[:motd_wiki_url] || config[:motd_wiki_url].empty?
+                bot.error "L'URL du MotD manque dans la configuration"
+            elsif ! config[:username] || config[:username].empty?
+                bot.error "Nom d'utilisateur sur le wiki manquant"
+            elsif ! config[:password] || config[:password].empty?
+                bot.error "Mot de passe du wiki manquant"
+            else
+                agent = login config[:motd_wiki_url], config[:username], config[:password]
+                updateMotD agent, config[:motd_wiki_url], msg.user, url
+                
+                state[:music][:changed] = Time.now
+                state[:music][:text] = url
+                makeTopic state, msg.channel
+                msg.reply "#{msg.user} gagne le MotD !"
+            end
+        end
+    end
+
+    match /topic/, :method => :changeTopic
+    def changeTopic msg
+        withState do |state|
+            state[:topic][:text] = msg.message.gsub /\!topic\s*/, ''
+            state[:topic][:changed] = Time.now
+            makeTopic state, msg.channel
         end
     end
 end
