b4f23cac26e9f990d26948e1a8eacb78436d1b37
iTitou <moiandme@gmail.com>
2015-12-02 20:12:50 +0100

    [enh] Mo4r urls kinds

--- plugins/urls.py
+++ plugins/urls.py
@@ -2,12 +2,25 @@ from .helpers import public_api, twitter
 
 
 def load(bot):
-    @bot.command(r'.*https?://github\.com/([\w\d_\.-]+)/([\w\d_\.-]+)/?(\s|$)')
+    github_repo = r'.*https?://github\.com/([\w\d_\.-]+)/([\w\d_\.-]+)'
+
+    @bot.command(github_repo + r'/?(\s|$)')
     def github(msg):
-        url = "https://api.github.com/repos/{}/{}".format(*msg.args[:2])
+        url = "https://api.github.com/repos/{}/{}".format(*msg.args)
         repo = yield from public_api(url)
-        fmt = "{name} [{language}] ({stargazers_count}*): «{description}»"
+        fmt = "@{name} [{language}] ({stargazers_count}*): «{description}»"
         msg.reply(fmt.format(**repo))
+
+    @bot.command(github_repo + r'/(issues|pull)/(\d+)')
+    def github_issue(msg):
+        user, repo, kind, id = msg.args
+        args = user, repo, id
+        url = "https://api.github.com/repos/{}/{}/issues/{}".format(*args)
+        issue = yield from public_api(url)
+        issue['author'] = issue['user']['login']
+        issue['labels'] = ' '.join('(%s)' % x['name'] for x in issue['labels'])
+        fmt = "@{author} [#{number}]: «{title}» {labels}"
+        msg.reply(fmt.format(**issue))
 
     @bot.command(r'.*https?://twitter.com/[^/]+/status/(\d+)')
     def twitter_status(msg):
