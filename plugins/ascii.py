5b124b696387b5064fb2ccf9a772cd94f6b79463
iTitou <moiandme@gmail.com>
2016-12-08 21:36:06 +0100

    [fix] Twitter url regexp

--- plugins/ascii.py
+++ plugins/ascii.py
@@ -5,3 +5,10 @@ class Ascii(BotPlugin):
     @BotPlugin.command(r'\!penisbird')
     def penisbird(self, msg):
         msg.reply(" < )\n ( \\\n.X\n8====D")
+
+
+    @BotPlugin.command(r'\!train')
+    def train(self, msg):
+        msg.reply("._||__|  |  ______   ______   ______ \n"
+                  "(        | |      | |      | |      |\n"
+                  "/-()---() ~ ()--() ~ ()--() ~ ()--()\n")
