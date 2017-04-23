47da0a4a507acd9232f6bfa0c2babb2e4cc3bfd1
iTitou <moiandme@gmail.com>
2017-04-23 16:06:01 +0200

    [fix] PEP8/Flakes + Lechbot is be_fr ;)

--- plugins/poll.py
+++ plugins/poll.py
@@ -1,6 +1,6 @@
 from ircbot.plugin import BotPlugin
-from collections import OrderedDict
 import asyncio
+
 
 class Poll(BotPlugin):
     def __init__(self):
@@ -8,28 +8,36 @@ class Poll(BotPlugin):
         self.votes = []
 
     def print_poll(self):
-        return "\n".join("%i) %s -- %s" % (i, vote["name"], str(vote["voters"])) for i, vote in enumerate(self.votes))
+        return "\n".join(
+            "%i) %s -- %s" % (i, vote["name"], ", ".join(vote["voters"]))
+            for i, vote in enumerate(self.votes)
+        )
 
     def create_poll(self, args, msg, time=10):
         if len(self.votes):
-            return "There is already a poll running."
+            return "Il y a déjà un sondage en cours"
 
         loop = asyncio.get_event_loop()
         loop.call_later(60 * time, self.end_poll, msg)
 
         if len(args) < 2:
-            return "More args needed: " + str(args)
+            return "Il manque des arguments: " + str(args)
 
-        for arg in args:#[:-1]:
+        for arg in args:  # [:-1]:
             self.votes.append({
                 "name": arg,
                 "voters": [],
             })
 
-        return "New poll created:\n" + self.print_poll()
+        return "\n".join([
+            "Nouveau sondage:\n",
+            self.print_poll(),
+            "Votez avec " + self.bot.text.bold("!vote <numero du choix>"),
+            self.bot.text.yellow("(fin dans %d minutes)") % time
+        ])
 
     def end_poll(self, msg):
-        msg.reply("Poll has ended\n" + self.print_poll())
+        msg.reply("Le sondage est terminé\n" + self.print_poll())
         self.votes = []
 
     @BotPlugin.command(r'\!poll (.+) (time=\d+)')
@@ -40,7 +48,9 @@ class Poll(BotPlugin):
         @param Une liste de nom à ajouter au sondage.
         @param Le temps en minute avant de cloturer le sondage.
         """
-        msg.reply(self.create_poll(msg.args[0].split(" "), msg, int(msg.args[1].replace("time=", ""))))
+        msg.reply(self.create_poll(msg.args[0].split(" "),
+                                   msg,
+                                   int(msg.args[1].replace("time=", ""))))
 
     @BotPlugin.command(r'\!poll (.+)')
     def poll(self, msg):
@@ -67,13 +77,13 @@ class Poll(BotPlugin):
         """
         index = int(msg.args[0])
         if not len(self.votes):
-            return msg.reply("No poll currently")
+            return msg.reply("Il n'y a pas de poll en cours")
         elif not 0 <= index < len(self.votes):
-            return msg.reply("Invalid poll response.")
+            return msg.reply("Choix invalide", hilight=True)
 
         if msg.user not in self.votes[index]["voters"]:
             self.votes[index]["voters"].append(msg.user)
         else:
-            return msg.reply("Already voted")
+            return msg.reply("Tu as déjà voté", hilight=True)
 
         msg.reply(self.print_poll())
