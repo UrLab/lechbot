78288a12583e57ef91ee743908a443477f8c78b7
Nikita Marchant <nikita.marchant@gmail.com>
2018-12-26 18:15:16 +0100

    French and doctrings

--- plugins/ccc35.py
+++ plugins/ccc35.py
@@ -17,7 +17,7 @@ class CCC35(BotPlugin):
         talk['start_time_dt'] = dateutil.parser.parse(talk['start_time'])
         talk['end_time_dt'] = dateutil.parser.parse(talk['end_time'])
         talk['relative_day'] = self.get_day(talk['start_time_dt'])
-        fmt = "«{title}» [Day {relative_day}, {start_time_dt:%H:%M}] in {room[name]}"
+        fmt = "«{title}» [Day {relative_day}, {start_time_dt:%H:%M}] dans {room[name]}"
         return fmt.format(**talk)
 
     def get_day(self, dt=None):
@@ -45,12 +45,14 @@ class CCC35(BotPlugin):
 
     @BotPlugin.command(r'\!day$')
     def day(self, msg):
+        """Quel jour du congrès sommes nous ? Day0 étant le 26/12/2018"""
         day = self.get_day()
-        fmt = "We are Day {day}"
+        fmt = "We are Day{day}"
         msg.reply(fmt.format(day=day))
 
     @BotPlugin.command(r'\!talks$')
     def talks(self, msg):
+        """Liste les talks en cours et les 5 suivants"""
         fahrplan = yield from self.get_fahrplan()
         events = fahrplan['conference_events']['events']
         events = sorted(events, key=lambda x: (dateutil.parser.parse(x['start_time']), dateutil.parser.parse(x['end_time'])))
@@ -61,34 +63,38 @@ class CCC35(BotPlugin):
         for event in events:
             if dateutil.parser.parse(event['start_time']) < now < dateutil.parser.parse(event['end_time']):
                 now_events.append(event)
-            elif now < dateutil.parser.parse(event['start_time']) < (now + timedelta(hours=3)):
+            elif now < dateutil.parser.parse(event['start_time']):
                 next_events.append(event)
 
         if now_events:
-            msg.reply("Current events:")
+            msg.reply("Talks en cours :")
             for event in now_events:
                 msg.reply(" * " + self.format_talk(event), strip_text=False)
 
         if next_events:
-            msg.reply("Next events:")
-            for event in next_events:
+            msg.reply("Prochains talks :")
+            for event in next_events[:5]:
                 msg.reply(" * " + self.format_talk(event), strip_text=False)
 
-    @BotPlugin.command(r'\!last_update$')
+    @BotPlugin.command(r'\!fahrplan$')
     def last_update(self, msg):
+        """Affiche le temps écoulé depuis la dernière mise à jour du Fahrplan"""
         fahrplan = yield from self.get_fahrplan()
         uptime = dateutil.parser.parse(fahrplan["update_time"])
         delta = datetime.now(self.TZ) - uptime
-        msg.reply("Fahrplan updated %i minutes ago" % (delta.total_seconds() // 60))
+        msg.reply("Fahrplan a été mis à jour il y a %i minutes" % (delta.total_seconds() // 60))
 
     @BotPlugin.command(r'\!update_fahrplan$')
     def update(self, msg):
-        msg.reply("Starting update...")
+        """Mettre à jour le Fahrplan"""
+        msg.reply("Mise à jour...")
         fahrplan = yield from self.get_fahrplan(force=True)
-        msg.reply("Updated Fahrplan")
+        msg.reply("Fahrplan mis à jour")
 
-    @BotPlugin.command(r'\!room (\w+)$')
+    @BotPlugin.command(r'\!salle (\w+)$')
     def room(self, msg):
+        """Liste les 5 prochains talks de la salle passée en paramètre
+        (voir !salles pour une liste des salles)"""
         fahrplan = yield from self.get_fahrplan()
         events = fahrplan['conference_events']['events']
         events = sorted(events, key=lambda x: (dateutil.parser.parse(x['start_time']), dateutil.parser.parse(x['end_time'])))
@@ -103,14 +109,15 @@ class CCC35(BotPlugin):
             for event in kept[:5]:
                 msg.reply(self.format_talk(event))
         else:
-            msg.reply("Room not found")
+            msg.reply("Salle introuvable")
 
-    @BotPlugin.command(r'\!rooms$')
+    @BotPlugin.command(r'\!salles$')
     def rooms(self, msg):
+        """Liste les salles de conférence"""
         fahrplan = yield from self.get_fahrplan()
         events = fahrplan['conference_events']['events']
 
         counter = Counter([e['room']['name'] for e in events])
 
         important_rooms = [name for name, count in counter.most_common(10) if count > 5]
-        msg.reply("Rooms: " + ", ".join(important_rooms))
+        msg.reply("Salles : " + ", ".join(important_rooms))
