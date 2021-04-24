99eefdf855eac3a07ed6ebc5752b3c84d4d04225
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-24 15:05:43 +0200

    [up] async in space.py

--- plugins/space.py
+++ plugins/space.py
@@ -10,14 +10,14 @@ class Space(BotPlugin):
     # @BotPlugin.command(r'\!poke')
     # def poke(self, msg):
     #     """Dit gentiment bonjour aux personnes présentes à UrLab"""
-    #     yield from lechbot_notif('poke')
+    #     await lechbot_notif('poke')
     #     msg.reply("Coucou HAL !!! /o/", hilight=True)
     #     self.bot.log.info('Poke by ' + msg.user)
 
     @BotPlugin.command(r"\!status")
-    def spacestatus(self, msg):
+    async def spacestatus(self, msg):
         """Affiche le statut actuel du hackerspace"""
-        space = yield from spaceapi()
+        space = await spaceapi()
 
         when = self.bot.naturaltime(space["state"]["lastchange"])
         if space["state"]["open"]:
@@ -26,10 +26,10 @@ class Space(BotPlugin):
             msg.reply("Le hackerspace a fermé " + when)
 
     @BotPlugin.command(r"sudo \!(open|close)")
-    def change_spacestatus(self, msg):
+    async def change_spacestatus(self, msg):
         """Change le statut du hackerspace en cas de dysfonctionnement de HAL"""
         status = msg.args[0]
-        yield from private_api(
+        await private_api(
             "/space/change_status", {"open": 1 if status == "open" else 0}
         )
         msg.reply("sudo request sent !")
