872ae28e8f6269768d66f9001eb2f2db89e5ef4b
Nikita Marchant <nikita.marchant@gmail.com>
2021-06-03 14:51:34 +0200

    Add sentry (#71)

--- lechbot.py
+++ lechbot.py
@@ -3,9 +3,10 @@ import logging
 from sys import stdout
 
 import humanize
+import sentry_sdk
 
 from chanconfig import CHANS
-from config import MAIN_CHAN, NICKNAME, PORT, SERVER
+from config import MAIN_CHAN, NICKNAME, PORT, SENTRY_DSN, SERVER
 from ircbot import CLIBot, IRCBot
 
 
@@ -13,6 +14,9 @@ def main(loglevel, klass, options):
     humanize.i18n.activate("fr_FR")
     fmt = "%(levelname)7s %(asctime)s | %(module)s:%(funcName)s | %(message)s"
     logging.basicConfig(stream=stdout, level=loglevel, format=fmt)
+
+    if SENTRY_DSN:
+        sentry_sdk.init(SENTRY_DSN, traces_sample_rate=1.0)
 
     bot = klass(NICKNAME, CHANS, main_chan=MAIN_CHAN, local_only=options.local)
 
